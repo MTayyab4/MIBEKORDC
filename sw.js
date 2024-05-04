3924f1974c0316dc590a4a936de4ae4d50690d6a
MTayyab4 <126855912+MTayyab4@users.noreply.github.com>
2024-05-04 20:06:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -113,86 +113,66 @@ const staticAssets = [
     cacheVersion++;
     cacheName = `cache-v${cacheVersion}`;
   }
-  
-  //Add cache while installing Sw
-  self.addEventListener("install", (event) => {
-    console.log("Attempting to install service worker and cache static assets");
-    event.waitUntil(
-      caches
-        .open(cacheName)
-        .then((cache) => {
-          //Update version
-          console.log("previous version", cacheVersion)
-          increment();
-          console.log("new version", cacheVersion)
-          //add files to the cache
-          console.log("adding cache" , cache.addAll(staticAssets))
-  
-          return cache.addAll(staticAssets);
-        })
-        .catch((err) => console.log(err))
-    );
+  importScripts('https://cdn.jsdelivr.net/npm/idb@4.0.3/build/iife/index-min.js');
+
+let db;
+
+const initializeDatabase = async () => {
+  db = await idb.openDB('yourDatabaseName', 1, {
+    upgrade(db) {
+      db.createObjectStore('staticAssets');
+    },
   });
-  
-  self.addEventListener("activate", (event) => {
-    console.log("Activating new service worker...");
-  
-    event.waitUntil(
-      caches.keys().then((cacheNames) => {
-        console.log("activating cacheName ", cacheName)
-        return Promise.all(
-          cacheNames.map((storedCacheName) => {
-            if (storedCacheName !== cacheName) {
-              console.log("Deleting old cache ", storedCacheName)
-              return caches.delete(storedCacheName);
-            }
-          })
-        );
+};
+
+const addToCache = async (url, response) => {
+  await db.put('staticAssets', response.clone(), url);
+  return response;
+};
+
+const getFromCache = async (request) => {
+  const cachedResponse = await db.get('staticAssets', request.url);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  return fetch(request);
+};
+
+self.addEventListener('install', (event) => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    initializeDatabase()
+      .then(() => console.log('Database initialized successfully'))
+      .catch((err) => console.error('Error initializing database:', err))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch event for', event.request.url);
+
+  event.respondWith(
+    getFromCache(event.request)
+      .then((response) => {
+        if (response) {
+          console.log('Found', event.request.url, 'in cache');
+          return response;
+        }
+
+        return fetch(event.request)
+          .then((response) => addToCache(event.request.url, response))
+          .catch((error) => {
+            console.error('Fetching Error:', error);
+            return caches.open(cacheName).then((cache) => cache.match('offline.html'));
+          });
       })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    console.log("Fetch event for", event.request.url);
-  
-    event.respondWith(
-      caches
-        .match(event.request)
-        .then((response) => {
-          //If the response is found in the cache
-          console.log("fetch response", response);
-          if (response) {
-            console.log("Found ", event.request.url, " in cache");
-            return response;
-          }
-  
-          return fetch(event.request).then((response) => {
-            // If a response is not found
-            console.log("fetch if (404) response", response);
-  
-            if (response.status === 404) {
-              return caches.open(cacheName).then((cache) => {
-                console.log("fetch return (404) response", response);
-  
-                return cache.match("404.html");
-              });
-            }
-  
-            //Caching and returning the response if it doesn't exist in the cache
-            return caches.open(cacheName).then((cache) => {
-              console.log("fetch cache.put & Cloning",cache)
-              cache.put(event.request.url, response.clone());
-              return response;
-            });
-          });
-        })
-        .catch(async (error) => {
-          console.log("Fetching Error, ", error);
-          //If page is offline/ Network failure
-          return caches.open(cacheName).then((cache) => {
-            console.log("Page not found ", cache)
-            return cache.match("offline.html");
-          });
-        })
-    );
-  });
+      .catch((error) => {
+        console.error('Error getting from cache:', error);
+        return caches.open(cacheName).then((cache) => cache.match('offline.html'));
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Activating new service worker...');
+});
